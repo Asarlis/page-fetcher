21d1fc8b80f479a8357dea9f6a01fc1e8dbba1b6
Sean Day <Asarlis55@gmail.com>
2021-05-06 17:47:36 +0000

    redone with stretch goals and fixed rl.close() issue

--- fetcher.js
+++ fetcher.js
@@ -18,18 +18,20 @@ request(domain, (error, response, body) => {
 
     if(error){
       console.log("This path is invalid", error);
+      rl.close()
     } 
     if (fs.existsSync(file)) {
       rl.question("Would you like to overwrite this file? y / n  ", (answer) =>{
         if (answer === "n"){
-          console.log("File was not overwritten")
+          console.log("File was not overwritten");
+          rl.close();
         }
         if (answer === "y") {
         console.log(`Downloaded and saved ${response.headers["content-length"]} bytes to ${file}`)
+        rl.close();
       }
     })
   }
-  rl.close();
   })
 })
 
